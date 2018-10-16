aa019b757c8e002ebb021bc3204a5c758e15982d
Thomas Kolonko <thomas.kolonko@ilub.unibe.ch>
2018-10-16 10:06:27 +0200

    new sql script

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -1,9 +1,23 @@
 <#1>
 <?php
-require_once "Customizing/global/plugins/Services/Cron/CronHook/TestCron/vendor/autoload.php";
+/** @var ilDB $ilDB */
+global $ilDB;
+$db = $ilDB;
+require_once('Customizing/global/plugins/Services/Cron/CronHook/TestCron/classes/class.ilTestCronPlugin.php');
 
-iLUB\Plugins\TestCron\Config\ArConfig::updateDB();
-
-$config = new \iLUB\Plugins\TestCron\Config\ArConfig();
-$config->save();
+if (! $db->tableExists(ilTestCronPlugin::TABLE_NAME)) {
+    $fields = array(
+        'expiration' => array(
+            'type' => 'integer',
+            'length' => 4,
+            'notnull' => TRUE
+        )
+    );
+    $db->createTable(ilTestCronPlugin::TABLE_NAME, $fields);
+    $db->insert(ilTestCronPlugin::TABLE_NAME, array(
+            ilTestCronPlugin::COLUMN_NAME => array(
+                    'integer', ilTestCronPlugin::DEFAULT_EXPIRATION_VALUE
+            )
+    ));
+}
 ?>
